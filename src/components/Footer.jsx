c942c5671fefb08226b6834125583a0c3f924bc3
Ashik~ <ashikibrahim7777@gmail.com>
2025-01-04 19:54:28 +0530

    Update Meta Tags and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,16 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; {new Date().getFullYear()} ReactIQ. All rights reserved.</p>
+        <p>
+          Developed by{" "}
+          <a
+            href="https://ashik-ibrahim.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ashik Ibrahim
+          </a>
+        </p>
       </div>
     </footer>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="react-logo.png" alt="React logo" />
+      <img src="react-logo.png" alt="React logo" width="100" height="100" />
       <h1>ReactIQ</h1>
     </header>
   );
